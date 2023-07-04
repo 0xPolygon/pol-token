5c152931be3734f663a9800936880377b4d16d3a
qedk <1994constant@gmail.com>
2023-07-05 03:50:22 +0400

    feat: fix impl

--- src/LinearInflationRateProvider.sol
+++ src/LinearInflationRateProvider.sol
@@ -5,17 +5,25 @@ import {IInflationRateProvider} from "./interfaces/IInflationRateProvider.sol";
 
 contract LinearInflationRateProvider is IInflationRateProvider {
     uint256 private immutable _mintPerSecond = 3170979198376458650;
+    uint256 public immutable endTimestamp;
     uint256 public immutable duration;
 
-    constructor(uint256 duration_) {
+    constructor(uint256 endTimestamp_, uint256 duration_) {
+        endTimestamp = endTimestamp_;
         duration = duration_;
     }
 
-    function getHubMintPerSecond() external view returns (uint256 hubMintPerSecond) {
-        hubMintPerSecond = _mintPerSecond * (duration - block.timestamp) / duration;
+    function getHubMintPerSecond() external view returns (uint256) {
+        if (block.timestamp > endTimestamp) {
+            return 0;
+        }
+        return (_mintPerSecond * (endTimestamp - block.timestamp) / duration);
     }
 
-    function getTreasuryMintPerSecond() external view returns (uint256 treasuryMintPerSecond) {
-        treasuryMintPerSecond = _mintPerSecond * (duration - block.timestamp) / duration;
+    function getTreasuryMintPerSecond() external view returns (uint256) {
+        if (block.timestamp > endTimestamp) {
+            return 0;
+        }
+        return (_mintPerSecond * (endTimestamp - block.timestamp) / duration);
     }
 }
