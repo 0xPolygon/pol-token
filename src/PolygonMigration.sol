b8479d7cfb00982dcc69f1a2cabd82a4b0092602
qedk <1994constant@gmail.com>
2023-07-05 03:23:16 +0400

    feat: remove ownable from migration contract

--- src/PolygonMigration.sol
+++ src/PolygonMigration.sol
@@ -3,9 +3,8 @@ pragma solidity 0.8.20;
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Ownable2Step} from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
 
-contract PolygonMigration is Ownable2Step {
+contract PolygonMigration {
     using SafeERC20 for IERC20;
 
     IERC20 public immutable polygon;
