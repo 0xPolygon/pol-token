e5f1769cd7eedd45b9b73b6dd7403f839da433c5
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2023-08-30 14:27:18 +0530

    chore: expand IPolygon to use IERC20

--- src/interfaces/IPolygon.sol
+++ src/interfaces/IPolygon.sol
@@ -2,7 +2,8 @@
 pragma solidity 0.8.21;
 
 import {IERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-interface IPolygon is IERC20Permit {
+interface IPolygon is IERC20, IERC20Permit {
     function mint(address to, uint256 amount) external;
 }
