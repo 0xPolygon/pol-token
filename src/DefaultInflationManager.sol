3b9abc29707a0184de5c9d205e7db695722035e2
gretzke <daniel@gretzke.de>
2023-09-01 18:01:24 +0200

    reset gap back to 50 as it hasn't been deployed yet

--- src/DefaultInflationManager.sol
+++ src/DefaultInflationManager.sol
@@ -108,5 +108,5 @@ contract DefaultInflationManager is
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[49] private __gap;
+    uint256[50] private __gap;
 }
