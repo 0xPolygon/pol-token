046cca15727bc7bdb80e9d181101dcb83aa5c075
Simon Dosch <sdosch@polygon.technology>
2024-12-06 17:33:42 +0100

    bump version 1.2.0

--- src/PolygonMigration.sol
+++ src/PolygonMigration.sol
@@ -98,7 +98,7 @@ contract PolygonMigration is Ownable2StepUpgradeable, IPolygonMigration {
 
     /// @inheritdoc IPolygonMigration
     function version() external pure returns (string memory) {
-        return "1.1.0";
+        return "1.2.0";
     }
 
     /// @inheritdoc IPolygonMigration
